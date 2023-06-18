05d9640ec819eb64d8703ef1580755be4764dafd
ModzabazeR <moddaeng.pps@gmail.com>
2023-06-18 15:31:51 +0700

    start working on store page

--- components/card.tsx
+++ components/card.tsx
@@ -1,12 +1,72 @@
-import ICard from "@/typings"
+import Image from "next/image";
+import { ICard } from "@/typings";
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
 
-export default function Card({name, type, description, price}: ICard) {
-	return (
-		<div className="bg-gray-200 mx-4 p-4">
-			<p>ชื่อการ์ด: {name}</p>
-			<p>ประเภท: {type}</p>
-			<p>ความสามารถ: {description}</p>
-			<p>ราคา: {price}</p>
-		</div>
-	)
-}
+const Card: React.FC<ICard> = ({
+  id,
+  name,
+  detail,
+  card_type_id,
+  price,
+  img_url,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const popupStyle = {
+    overlay: {
+      backgroundColor: "rgba(0, 0, 0, 0.75)",
+    },
+    content: {
+      background: "#D9D9D9",
+    },
+  };
+
+  const openPopup = () => {
+    setIsOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    console.log("Card Id: " + id);
+  }, []);
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closePopup}
+        contentLabel="Test Modal"
+        closeTimeoutMS={200}
+        style={popupStyle}
+      >
+        <div className="flex flex-col items-center text-center h-full w-full justify-center">
+          <Image src={img_url} alt={name} width={200} height={400} />
+          <p className="text-xl font-bold my-2">{name}</p>
+          <p>ประเภท: {card_type_id}</p>
+          <p>
+            ความสามารถ:
+            <br />
+            {detail}
+          </p>
+          <p>ราคา: {price}$</p>
+		  <div className="flex w-full justify-between mt-2">
+			  <button className="bg-[#ACACAC] px-4 py-2 w-1/2 rounded-l-lg" onClick={closePopup}>ออก</button>
+			  <button className="bg-[#F90000] px-4 py-2 w-1/2 rounded-r-lg">ซื้อ</button>
+		  </div>
+        </div>
+      </Modal>
+
+      <div
+        className="drop-shadow-md text-center rounded-lg"
+        onClick={openPopup}
+      >
+        <Image src={img_url} alt={name} width={200} height={400} />
+        <p className="text-2xl text-white leading-10">{price}$</p>
+      </div>
+    </>
+  );
+};
+
+export default Card;
---
+++ pages/store.tsx
@@ -0,0 +1,58 @@
+import { GetServerSideProps, NextPage } from "next";
+import Link from "next/link";
+import Card from "@/components/card";
+import { ICard } from "@/typings";
+
+const Store: NextPage<{ cardArr: ICard[] }> = ({ cardArr }) => {
+  return (
+    <div className="flex flex-col items-center bg-[#555555] py-6 gap-4">
+      <Link
+        href="/"
+        className="absolute bg-blue-600 py-2 px-5 text-white right-5 top-5"
+      >
+        Back
+      </Link>
+      <h1 className="text-5xl text-white">ร้านค้า</h1>
+      <div className="grid grid-cols-2 gap-4 p-4">
+        {cardArr.map((e, i) => {
+          return (
+            <Card
+              key={i}
+              id={e.id}
+              name={e.name}
+              detail={e.detail}
+              card_type_id={e.card_type_id}
+              price={e.price}
+              img_url={e.img_url}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Store;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tempCard = {
+    id: "1",
+    name: "Example Card",
+    detail: "This is an Example Card",
+    card_type_id: 0,
+    price: 50,
+    img_url:
+      "https://1.bp.blogspot.com/-agFXet3-wqg/VBTsSI1HkrI/AAAAAAAACWg/4lKOvcczY6E/s1600/การ์ดนางฟ้า.jpg",
+  };
+
+  const cardArr = [];
+  for (let i = 0; i < 10; i++) {
+    cardArr.push(tempCard);
+  }
+
+  return {
+    props: {
+      cardArr,
+    },
+  };
+};
