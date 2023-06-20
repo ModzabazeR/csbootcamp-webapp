287c1826efafe01d5a7218be1d715b5a62679213
ModzabazeR <moddaeng.pps@gmail.com>
2023-06-20 20:07:15 +0700

    trying to fix font!!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Modal from "react-modal";
 
-// import { Kanit } from "next/font/google";
+import { Kanit } from "next/font/google";
 import Head from "next/head";
 
-// const kanit = Kanit({
-//   subsets: ["thai", "latin"],
-//   weight: ["100", "400", "700"], // Thin, Regular, Bold
-//   style: ["normal", "italic"],
-//   display: "swap",
-// });
+const kanit = Kanit({
+  subsets: ["thai", "latin"],
+  weight: ["100", "400", "700"], // Thin, Regular, Bold
+  style: ["normal", "italic"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   Modal.setAppElement("#__next");
@@ -18,18 +18,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>CS Bootcamp 2023 | 29 Ways to Survive</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Kanit&display=swap"
-          rel="stylesheet"
-        />
       </Head>
-      <Component {...pageProps} />
+      <main className={kanit.className}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
