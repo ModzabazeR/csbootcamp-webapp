df1aca65f0470d823ae711750649eac3e9dc93b9
ModzabazeR <moddaeng.pps@gmail.com>
2023-06-21 00:10:13 +0700

    adjust back button

--- pages/store.tsx
+++ pages/store.tsx
@@ -3,20 +3,22 @@ import Link from "next/link";
 import Card from "@/components/card";
 import { ICard, getCardsResponse } from "@/typings";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 const Store: NextPage<{ cardArr: ICard[] }> = ({ cardArr }) => {
+  const router = useRouter()
   console.log(cardArr)
   return (
     <div className="flex flex-col items-center bg-[#555555] py-6 gap-4">
       <Head>
         <title>Shop | CS Bootcamp 2023</title>
       </Head>
-      <Link
-        href="/"
-        className="absolute bg-blue-600 py-2 px-5 text-white right-5 top-5"
+      <div
+        onClick={() => router.back()}
+        className="absolute bg-blue-600 py-2 px-5 text-white right-5 top-5 cursor-pointer"
       >
         Back
-      </Link>
+      </div>
       <h1 className="text-5xl text-white">ร้านค้า</h1>
       <div className="grid grid-cols-2 gap-4 p-4">
         {cardArr.map((e, i) => {
